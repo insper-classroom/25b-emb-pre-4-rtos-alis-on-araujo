58fa3172979392c4ea9c78a8fb7401218278714c
Alison Araujo <alisonao@al.insper.edu.br>
2025-10-06 15:01:17 -0300

    exe1

--- exe1/main.c
+++ exe1/main.c
@@ -23,6 +23,19 @@ void led_1_task(void *p) {
   }
 }
 
+void led_2_task(void *p) {
+  gpio_init(LED_PIN_G);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+  int delay = 500;
+  while (true) {
+    gpio_put(LED_PIN_G, 1);
+    vTaskDelay(pdMS_TO_TICKS(delay));
+    gpio_put(LED_PIN_G, 0);
+    vTaskDelay(pdMS_TO_TICKS(delay));
+  }
+}
+
 int main() {
   stdio_init_all();
   printf("Start RTOS \n");
